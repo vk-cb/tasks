fdd11ba88cfb90ca59dda7be84d49bdae083f8a4
Vicky Kumar Gupta <vickygupta031@gmail.com>
2025-02-18 23:43:20 +0530

    some bug fixes

--- src/controllers/admin/adminController.ts
+++ src/controllers/admin/adminController.ts
@@ -3,138 +3,221 @@ import { statusCodes } from "../../utility/statusCodes";
 import users from "../../models/users";
 import tasks from "../../models/tasks";
 
-
-export const usersListController = async (req: Request, res: Response, next: NextFunction):Promise<void> => {
-    try {
-        const payload = req.body;
-        const adminData = req.user;
-        if(!adminData){
-            res.status(statusCodes.unauthorized).json({ msg: "User not authenticated" });
-            return;
-        }
-        else {
-            if(adminData.role !== "admin"){
-                res.status(statusCodes.unauthorized).json({ msg: "User not authenticated" });
-                return;
-            }
-            else {
-                if(payload.isActive){
-                    const usersList = await users.find({ role: "user", isActive : payload.isActive });
-                     const sortedUserList = usersList.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-
-                    res.status(statusCodes.success).json( { msg: "Users fetched successfully", data: sortedUserList, noOfBlockedUsers : usersList.length });
-                    return;
-                }else{
-                    const usersList = await users.find({ role: "user", isActive : true });
-                    const sortedUserList = usersList.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-
-                    res.status(statusCodes.success).json({ msg: "Users fetched successfully", data: sortedUserList, noOfUsers : usersList.length });
-                    return;
-                }
-            }
-        }
-        
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        res.status(statusCodes.internalServerError).json({ msg: "Internal Server Error" });
+export const usersListController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const payload = req.body;
+    const adminData = req.user;
+    if (!adminData) {
+      res
+        .status(statusCodes.unauthorized)
+        .json({ msg: "User not authenticated" });
+      return;
+    } else {
+      if (adminData.role !== "admin") {
+        res
+          .status(statusCodes.unauthorized)
+          .json({ msg: "User not authenticated" });
         return;
+      } else {
+        if (payload.isActive) {
+          const usersList = await users.find(
+            { role: "user", isActive: payload.isActive },
+            { password: 0 }
+          );
+          const sortedUserList = usersList.sort(
+            (a, b) =>
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
+
+          res
+            .status(statusCodes.success)
+            .json({
+              msg: "Users fetched successfully",
+              data: sortedUserList,
+              noOfBlockedUsers: usersList.length,
+            });
+          return;
+        } else {
+          const usersList = await users.find(
+            { role: "user", isActive: true },
+            { password: 0 }
+          );
+          const sortedUserList = usersList.sort(
+            (a, b) =>
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
+
+          res
+            .status(statusCodes.success)
+            .json({
+              msg: "Users fetched successfully",
+              data: sortedUserList,
+              noOfUsers: usersList.length,
+            });
+          return;
+        }
+      }
     }
-}
-export const activateDeactivateUserController = async (req: Request, res: Response, next: NextFunction):Promise<void> => {
-
-    try {
-        const { id } = req.params;
-        const user = await users.findById(id);
-        const admin = req.user;
-        if(admin.role !== "admin"){
-            res.status(statusCodes.unauthorized).json({ msg: "You are not authorized" });
-            return;
-        }
-        if (!user) {
-            res.status(statusCodes.notFound).json({ msg: "User not found" });
-            return;
-        }
-        user.isActive = !user.isActive;
-        await user.save();
-        res.status(statusCodes.success).json({ msg: "User updated successfully", data: user });
-        return;
-    } catch (error) {
-        console.error("Error updating user:", error);
-        res.status(statusCodes.internalServerError).json({ msg: "Internal Server Error" });
-        return;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res
+      .status(statusCodes.internalServerError)
+      .json({ msg: "Internal Server Error" });
+    return;
+  }
+};
+export const activateDeactivateUserController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const user = await users.findById(id);
+    const admin = req.user;
+    if (admin.role !== "admin") {
+      res
+        .status(statusCodes.unauthorized)
+        .json({ msg: "You are not authorized" });
+      return;
     }
-}
+    if (!user) {
+      res.status(statusCodes.notFound).json({ msg: "User not found" });
+      return;
+    }
+    user.isActive = !user.isActive;
+    await user.save();
+    res.status(statusCodes.success).json({ msg: "User updated successfully" });
+    return;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res
+      .status(statusCodes.internalServerError)
+      .json({ msg: "Internal Server Error" });
+    return;
+  }
+};
 
-export const userWiseTaskListController = async (req: Request, res: Response, next: NextFunction):Promise<void> => {
-   try {
+export const userWiseTaskListController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
     const userId = req.params.id;
     const admin = req.user;
-    if(admin.role !== "admin"){
-        res.status(statusCodes.unauthorized).json({
-            msg : "You are not Authorized to access"
-        })
-        return;
+    if (admin.role !== "admin") {
+      res.status(statusCodes.unauthorized).json({
+        msg: "You are not Authorized to access",
+      });
+      return;
     }
-    const userTasks = await tasks.find({user : userId});
-    
-    if(!userTasks){
-        res.status(statusCodes.notFound).json({
-            msg : "User not found"
-        })
-        return;
-    } 
-    const sortedTasks = userTasks.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+    const userTasks = await tasks.find({ user: userId });
+
+    if (!userTasks) {
+      res.status(statusCodes.notFound).json({
+        msg: "User not found",
+      });
+      return;
+    }
+    const sortedTasks = userTasks.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
     res.status(statusCodes.success).json({
-        msg : "Task fetched successfully",
-        data : sortedTasks
-    })
+      msg: "Task fetched successfully",
+      data: sortedTasks,
+    });
     return;
-   } catch (error) {
-    console.log(error)
+  } catch (error) {
+    console.log(error);
     return;
-   }
-}      
+  }
+};
 
-export const deleteUserTaskController = async (req: Request, res: Response, next: NextFunction):Promise<void> => {
-    try {
-        const taskId = req.params.id;
-        const admin = req.user;
-        if(admin.role !== "admin"){
-            res.status(statusCodes.unauthorized).json({ msg: "You are not authorized" });
-            return;
-        }
-        const deletedTask = await tasks.findOneAndUpdate({ _id: taskId, isActive: true }, { isActive: false }, { new: true });
-        if (!deletedTask) {
-            res.status(statusCodes.notFound).json({ msg: "Task not found" });
-            return;
-        }
-        res.status(statusCodes.success).json({ msg: "Task deleted successfully", data: deletedTask });
-        return;
+export const deleteUserTaskController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const taskId = req.params.id;
+    const admin = req.user;
+    if (admin.role !== "admin") {
+      res
+        .status(statusCodes.unauthorized)
+        .json({ msg: "You are not authorized" });
+      return;
     }
-    catch (error) {
-        console.error("Error deleting task:", error);
-        res.status(statusCodes.internalServerError).json({ msg: "Internal Server Error" });
-        return;
+    const deletedTask = await tasks.findOneAndUpdate(
+      { _id: taskId, isActive: true },
+      { isActive: false },
+      { new: true }
+    );
+    if (!deletedTask) {
+      res.status(statusCodes.notFound).json({ msg: "Task not found" });
+      return;
     }
-}
+    res
+      .status(statusCodes.success)
+      .json({ msg: "Task deleted successfully", data: deletedTask });
+    return;
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    res
+      .status(statusCodes.internalServerError)
+      .json({ msg: "Internal Server Error" });
+    return;
+  }
+};
 
-export const updateAdminTaskController = async(req : Request, res : Response, next: NextFunction) : Promise<void> =>{
-    try {
-        const taskId = req.params.id;
-        const admin = req.user; 
-        if(req.user.role !== "admin"){
-            res.status(statusCodes.unauthorized).json({ status: statusCodes.unauthorized, msg: "You are not authorized" });
-            return;
-        }
-        const updatedTask = await tasks.findOneAndUpdate({_id : taskId, isActive :true }, req.body, {new : true});
-        if(!updatedTask){
-            res.status(statusCodes.notFound).json({ status : statusCodes.notFound, msg: "Task not found" });
-            return;
-        }
-        res.status(statusCodes.success).json({status : statusCodes.success, msg: "Task updated successfully", data: updatedTask });
-        return;
-    } catch (error) {
-        res.status(statusCodes.internalServerError).json({ status: statusCodes.internalServerError, msg: "Internal Server Error" });
-        return;
+export const updateAdminTaskController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const taskId = req.params.id;
+    const admin = req.user;
+    if (req.user.role !== "admin") {
+      res
+        .status(statusCodes.unauthorized)
+        .json({
+          status: statusCodes.unauthorized,
+          msg: "You are not authorized",
+        });
+      return;
     }
-}
+    const updatedTask = await tasks.findOneAndUpdate(
+      { _id: taskId, isActive: true },
+      req.body,
+      { new: true }
+    );
+    if (!updatedTask) {
+      res
+        .status(statusCodes.notFound)
+        .json({ status: statusCodes.notFound, msg: "Task not found" });
+      return;
+    }
+    res
+      .status(statusCodes.success)
+      .json({
+        status: statusCodes.success,
+        msg: "Task updated successfully",
+        data: updatedTask,
+      });
+    return;
+  } catch (error) {
+    res
+      .status(statusCodes.internalServerError)
+      .json({
+        status: statusCodes.internalServerError,
+        msg: "Internal Server Error",
+      });
+    return;
+  }
+};
--- src/controllers/user/auth.ts
+++ src/controllers/user/auth.ts
@@ -5,51 +5,90 @@ import { Request, Response } from "express";
 import { statusCodes } from "../../utility/statusCodes";
 import dotenv from "dotenv";
 import roles from "../../models/roles";
-dotenv.config(); 
-const secret = process.env.JWT_SECRET    
+dotenv.config();
+const secret = process.env.JWT_SECRET;
 if (!secret) {
-    throw new Error("Server Error,  Environment variable JWT_SECRET is not defined");
-  }
-  
-export const userSignUpController = async (req : Request, res:Response,)=>{
-    console.log(process.env.JWT_SECRET)
-    try {
-        const {email, password, name, role } = req.body;
-        const findUser = await users.findOne({email})
-        if(findUser){
-          res.send({msg : "User is already exist for this email adderss use another email"})
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser =new users({email, password : hashedPassword, name, role}) 
-        // console.log(newUser)
-       await newUser.save()
-       
-        const token = jwt.sign({ id: newUser._id, name: newUser.name, email: newUser.email, role: role,   }, secret);
-         res.status(statusCodes.success).json({status: statusCodes.success, msg: "User created successfully",  token , data : { id: newUser._id, name: newUser.name, email: newUser.email, role: role,  createdAt: newUser.createdAt, updatedAt: newUser.updatedAt } });
-         return;
-    } catch (error) {
-        res.status(statusCodes.internalServerError).send(error), {status : statusCodes.internalServerError, msg: "Internal Server Error" };
-        return;
-    }
+  throw new Error(
+    "Server Error,  Environment variable JWT_SECRET is not defined"
+  );
 }
 
-export const userLoginController = async (req: Request, res: Response): Promise<void> => {
+export const userSignUpController = async (req: Request, res: Response) => {
+  console.log(process.env.JWT_SECRET);
+  try {
+    const { email, password, name, role } = req.body;
+    const findUser = await users.findOne({ email });
+    if (findUser) {
+      res.send({
+        msg: "User is already exist for this email adderss use another email",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let isActive = false;
+    if (role === "user") {
+      isActive = true;
+    } else {
+      isActive = false;
+    }
+    const newUser = new users({
+      email,
+      password: hashedPassword,
+      name,
+      role,
+      isActive: isActive,
+    });
+    // console.log(newUser)
+    await newUser.save();
+
+    const token = jwt.sign(
+      { id: newUser._id, name: newUser.name, email: newUser.email, role: role },
+      secret
+    );
+    res.status(statusCodes.success).json({
+      status: statusCodes.success,
+      msg: "User created successfully",
+      token,
+      data: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: role,
+        createdAt: newUser.createdAt,
+        updatedAt: newUser.updatedAt,
+      },
+    });
+    return;
+  } catch (error) {
+    res.status(statusCodes.internalServerError).send(error),
+      { status: statusCodes.internalServerError, msg: "Internal Server Error" };
+    return;
+  }
+};
+
+export const userLoginController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const { email, password } = req.body;
 
     // Find the user by email
-    const findUser:any = await users.findOne({ email });
-    if(findUser.isActive === false){
-      res.status(statusCodes.forbidden).send({ msg: "Your account is blocked. Please contact the admin." });
-      return
+    const findUser: any = await users.findOne({ email });
+    if (findUser.isActive === false) {
+      res
+        .status(statusCodes.forbidden)
+        .send({
+          msg: "Your account is not active. Please contact the super-admin to active the account.",
+        });
+      return;
     }
     // If user does not exist, send a response
     if (!findUser) {
-       res
-        .status(statusCodes.notFound)
-        .send({ msg: "User does not exist for this email address. Use another email." });
-        return;
+      res.status(statusCodes.notFound).send({
+        msg: "User does not exist for this email address. Use another email.",
+      });
+      return;
     }
 
     // Compare the password
@@ -57,10 +96,8 @@ export const userLoginController = async (req: Request, res: Response): Promise<
 
     // If password is invalid, send a response
     if (!isMatch) {
-      res
-        .status(statusCodes.unauthorized)
-        .send({ msg: "Invalid password" });
-        return;
+      res.status(statusCodes.unauthorized).send({ msg: "Invalid password" });
+      return;
     }
 
     // Generate a JWT token
@@ -68,24 +105,31 @@ export const userLoginController = async (req: Request, res: Response): Promise<
       { id: findUser._id, name: findUser.name, email: findUser.email },
       secret
     );
-// payload
-const payload = {
-  id: findUser._id,
-  name: findUser.name,
-  email: findUser.email,
-  role: findUser.role,
-  createdAt: findUser.createdAt,
-  updatedAt: findUser.updatedAt
-};
-   
+    // payload
+    const payload = {
+      id: findUser._id,
+      name: findUser.name,
+      email: findUser.email,
+      role: findUser.role,
+      createdAt: findUser.createdAt,
+      updatedAt: findUser.updatedAt,
+    };
+
     // Respond with success
-     res
-      .status(statusCodes.success)
-      .json({status: statusCodes.success, msg: "User logged in successfully", token , data : payload });
-      return;
+    res.status(statusCodes.success).json({
+      status: statusCodes.success,
+      msg: "User logged in successfully",
+      token,
+      data: payload,
+    });
+    return;
   } catch (error) {
     console.error("Error in user login:", error);
-    res.status(statusCodes.internalServerError).send({status : statusCodes.internalServerError, msg: "Internal Server Error", error });
+    res.status(statusCodes.internalServerError).send({
+      status: statusCodes.internalServerError,
+      msg: "Internal Server Error",
+      error,
+    });
     return;
   }
 };
